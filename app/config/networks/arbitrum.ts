7b5e583bd80d344bf50d98a6f299b427c03a3ccf
Eli <eli.sakov@hotmail.com>
2022-08-24 21:14:26 -0700

    add uni & link

--- app/config/networks/arbitrum.ts
+++ app/config/networks/arbitrum.ts
@@ -7,20 +7,6 @@ export default {
     PriceOracle: "0x4F6D68130140578d2C071f210b544c27923E133d",
   },
   Tokens: {
-    // DAI: {
-    //   name: "DAI",
-    //   symbol: "DAI",
-    //   decimals: 18,
-    //   address: "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1",
-    //   icon: "/images/coin-icons/dai.svg",
-    //   priceDecimals: 30,
-    //   cToken: {
-    //     name: "tDAI",
-    //     symbol: "tDAI",
-    //     decimals: 8,
-    //     address: "0xeA4D2fF197fDf5d74554C17aCb720b9fA25D7cB7",
-    //   },
-    // },
 
     USDC: {
       name: "USDC",
@@ -49,8 +35,6 @@ export default {
         symbol: "tETH",
         decimals: 8,
         address: "0x7C0beDaF3aD27E50dF2810B4611B5222e12696Dc"
-        // address: "0x39D3C99F3B8b86C44aAe49EAdaBab3b00f106FED"
-        // address: "0x39D3C99F3B8b86C44aAe49EAdaBab3b00f106FED",
       },
     },
 
@@ -90,36 +74,36 @@ export default {
       },
     },
 
-    // IWBTC: {
-    //   name: "IwBTC",
-    //   symbol: "IWBTC",
-    //   decimals: 8,
-    //   address: "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f",
-    //   icon: "/images/coin-icons/bitcoin.svg",
-    //   priceDecimals: 30,
-    //   cToken: {
-    //     name: "tWBTC",
-    //     symbol: "tWBTC",
-    //     decimals: 8,
-    //     // address: "0xb8Cc8EF4998796Cc940d3c6C64104f990e5620b5", delegator
-    //     address: "0x5EC9A72e2987CE907Ad78724e83927d8C66A4555",
-    //   },
-    // },
-        // };
+    LINK: {
+      name: "tLINK",
+      symbol: "tLINK",
+      decimals: 18,
+      address: "0xf97f4df75117a78c1A5a0DBb814Af92458539FB4",
+      icon: "/images/coin-icons/link.svg",
+      priceDecimals: 28,
+      cToken: {
+        name: "tLINK",
+        symbol: "tLINK",
+        decimals: 8,
+        address: "0x07Fc3fC969d938Bd34EA506fC85eb3A62627F23A",
+      },
+    },
 
 
-// {
-//     "PriceOracle": "0x61706d8e55A41Ef1a652c2Aba2D072D27Bf21B41",
-//     "Unitroller": "0x84A57414910FBfE9f5503bFCA2F1e31B6Ad0981A",
-//     "Comptroller": "0xf38Cc17F8857290623BaD04911438d82586cA9DF",
-//     "IRModels": {
-//       "JumpRateModelV2": {
-//         "0__80__50__1000": "0x8081100BA341f2d4962c6fbeBD82cA0F49dC511E"
-//       }
-//     },
-//     "tDAIC": "0xaa604683c10eF6BE9fbA2add769442224Ef3cB20",
-//     "tWBTC": "0x549580b92fBf534277Bc328bd8C12f0e9C2bA344",
-//     "fsGLP": "0x0D830E0012eCD47652ED1C2b0d9DbB5C93c4d3Ff"
-//   }
+    UNI: {
+      name: "UNI",
+      symbol: "UNI",
+      decimals: 18,
+      address: "0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0",
+      icon: "/images/coin-icons/uni.svg",
+      priceDecimals: 28,
+      cToken: {
+        name: "tUNI",
+        symbol: "tUNI",
+        decimals: 8,
+        address: "0x423dB304E318693EB14c891B104AcB3af35fC69f",
+      },
+    },
+
   }
 }
