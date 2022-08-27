c38866571e3311946013c4b4ffcdf36687220979
Eli <eli.sakov@hotmail.com>
2022-08-27 11:58:12 -0700

    change token order

--- app/config/networks/arbitrum.ts
+++ app/config/networks/arbitrum.ts
@@ -9,34 +9,34 @@ export default {
   },
   Tokens: {
 
-    FRAX: {
-      name: "FRAX",
-      symbol: "FRAX",
+
+    ETH: {
+      name: "ETH",
+      symbol: "ETH",
       decimals: 18,
-      address: "0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F",
-      icon: "/images/coin-icons/frax.svg",
-      priceDecimals: 28,
+      //   address: eth has no address
+      icon: "/images/coin-icons/ethereum.svg",
+      priceDecimals: 18,
       cToken: {
-        name: "tFRAX",
-        symbol: "tFRAX",
+        name: "tETH",
+        symbol: "tETH",
         decimals: 8,
-        address: "0x86356683eca061FA3dD795aF3A22a1530a999b58",
+        address: "0x3EfFa48cB7c65399676D49f4B08696151f2446CC"
       },
     },
 
-
-    DAI: {
-      name: "DAI",
-      symbol: "DAI",
-      decimals: 18,
-      address: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
-      icon: "/images/coin-icons/dai.svg",
+    WBTC: {
+      name: "wBTC",
+      symbol: "WBTC",
+      decimals: 8,
+      address: "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f",
+      icon: "/images/coin-icons/bitcoin.svg",
       priceDecimals: 28,
       cToken: {
-        name: "tDAI",
-        symbol: "tDAI",
+        name: "tWBTC",
+        symbol: "tWBTC",
         decimals: 8,
-        address: "0x916b44509CcfC5238f8Ce9a30bEB1BF861B70779",
+        address: "0x29D4Cf28db3f978591F9868006BD3c5D2f36801f",
       },
     },
 
@@ -72,35 +72,56 @@ export default {
     },
 
 
-    ETH: {
-      name: "ETH",
-      symbol: "ETH",
+
+    DAI: {
+      name: "DAI",
+      symbol: "DAI",
       decimals: 18,
-      //   address: eth has no address
-      icon: "/images/coin-icons/ethereum.svg",
-      priceDecimals: 18,
+      address: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
+      icon: "/images/coin-icons/dai.svg",
+      priceDecimals: 28,
       cToken: {
-        name: "tETH",
-        symbol: "tETH",
+        name: "tDAI",
+        symbol: "tDAI",
         decimals: 8,
-        address: "0x3EfFa48cB7c65399676D49f4B08696151f2446CC"
+        address: "0x916b44509CcfC5238f8Ce9a30bEB1BF861B70779",
+      },
+    },
+    FRAX: {
+      name: "FRAX",
+      symbol: "FRAX",
+      decimals: 18,
+      address: "0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F",
+      icon: "/images/coin-icons/frax.svg",
+      priceDecimals: 28,
+      cToken: {
+        name: "tFRAX",
+        symbol: "tFRAX",
+        decimals: 8,
+        address: "0x86356683eca061FA3dD795aF3A22a1530a999b58",
       },
     },
 
-    WBTC: {
-      name: "wBTC",
-      symbol: "WBTC",
-      decimals: 8,
-      address: "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f",
-      icon: "/images/coin-icons/bitcoin.svg",
+
+
+
+    UNI: {
+      name: "UNI",
+      symbol: "UNI",
+      decimals: 18,
+      address: "0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0",
+      icon: "/images/coin-icons/uni.svg",
       priceDecimals: 28,
       cToken: {
-        name: "tWBTC",
-        symbol: "tWBTC",
+        name: "tUNI",
+        symbol: "tUNI",
         decimals: 8,
-        address: "0x29D4Cf28db3f978591F9868006BD3c5D2f36801f",
+        address: "0x75095636CD74FdDA8bC36a2bdC455489C86B30bf",
       },
     },
+
+
+
 
     LINK: {
       name: "LINK",
@@ -118,20 +139,6 @@ export default {
     },
 
 
-    UNI: {
-      name: "UNI",
-      symbol: "UNI",
-      decimals: 18,
-      address: "0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0",
-      icon: "/images/coin-icons/uni.svg",
-      priceDecimals: 28,
-      cToken: {
-        name: "tUNI",
-        symbol: "tUNI",
-        decimals: 8,
-        address: "0x75095636CD74FdDA8bC36a2bdC455489C86B30bf",
-      },
-    },
 
   },
 
