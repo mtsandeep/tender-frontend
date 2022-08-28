abf3bd803600c6d91013ab735098cc3bffea2ff6
Stefan <stefanv407@gmail.com>
2022-08-28 18:04:56 +0300

    added market page

---
+++ app/components/two-panels/tooltip-mibile-MULTI.tsx
@@ -0,0 +1,37 @@
+import ReactModal from "react-modal";
+
+export default function TooltipMobileMulti({ ...props }) {
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={true}
+      isOpen={props.tooltipData.open}
+      onRequestClose={() => props.handleClose()}
+      portalClassName="modal"
+      style={{
+        content: {
+          margin: "20px auto",
+          position: "relative",
+          maxWidth: 300,
+        },
+      }}
+    >
+      <div className="justify-self-center w-full h-full bg-[#181D1B] shadow-lg rounded-[10px] pt-[16px] pr-[30px] pb-[2px] pl-[20px] lg:hidden">
+        {props.tooltipData?.coins.map((coin: any, index: number) => {
+          return (
+            <div key={index} className="flex justify-between mb-[14px]">
+              <div className="flex gap-[8px]">
+                <img className="max-w-[18px]" src={coin.iconSrc} alt="..." />
+                <span className="font-nova text-white text-sm font-normal">
+                  {coin.coinTitle}
+                </span>
+              </div>
+              <span className="font-nova text-white text-sm font-normal">
+                {coin.data}
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    </ReactModal>
+  );
+}
