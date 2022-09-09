5c2886b3cb1db711c878468e7f36fce32cc43a4f
Stefan <stefanv407@gmail.com>
2022-09-10 01:19:51 +0300

    fixed menu

--- app/components/header-conponents/Header.tsx
+++ app/components/header-conponents/Header.tsx
@@ -75,7 +75,7 @@ export default function Header() {
               <img src="/images/logo1.svg" alt="Tender Finance" />
             </a>
           </div>
-          <div className="text-[#ADB5B3] hidden lg:flex justify-center font-normal text-base font-nova gap-x-[20px] xl:gap-x-[30px] z-20 absolute left-[240px] top-[50%] translate-x-[0] translate-y-[-50%] xl:translate-x-[-50%] xl:left-[50%]">
+          <div className="text-[#ADB5B3] hidden lg:flex justify-center font-normal text-base font-nova gap-x-[20px] xl:gap-x-[30px] z-20 absolute lg:left-[300px] top-[50%] translate-x-[0] translate-y-[-50%] 2xl:translate-x-[-50%] 2xl:left-[50%]">
             {menuLinks.map((item: { name: string; link: string }) => (
               <a
                 key={item.name}
