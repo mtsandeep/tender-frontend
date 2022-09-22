a8a6487a1303b5c6e58b9611221a32d893246033
Stefan <stefanv407@gmail.com>
2022-09-22 13:45:36 +0300

    fixed loader the Earn page

--- app/components/earn-page/emptyEarnContent.tsx
+++ app/components/earn-page/emptyEarnContent.tsx
@@ -23,74 +23,50 @@ export default function EmptyEarnContent() {
           Tokens: 0.21 TND, 56.43 esTND, 15.93 MP.
         </p>
         <div className="font-nova w-full">
-          <div key="1" className="panel-custom">
+          <div className="panel-custom">
             <div className="font-space text-lg md:text-[20px] leading-[23px] md:leading-[26px] px-[15px] py-[19px] md:px-[30px] md:pt-[23px] md:pb-[20px]  border-b-[1px] border-[#282C2B] border-solid px-[15px] uppercase">
               TENDIES
             </div>
             <div className="px-[15px] pt-[20px] pb-[16.9px] md:px-[30px] md:pt-[24px] md:pb-[30px] text-sm leading-5 md:text-base md:leading-[22px]">
-              <div
-                key="1"
-                className="border-[#282C2B] border-b-[1px] flex flex-col gap-y-[12px] md:gap-y-[15px] pb-[20px] md:pb-[24px]"
-              >
-                <div className="animate w-full h-[20px] md:h-[22px]"></div>
-                <div className="animate w-full h-[20px] md:h-[22px]"></div>
-                <div className="animate w-full h-[20px] md:h-[22px]"></div>
+              <div className="border-[#282C2B] border-b-[1px] flex flex-col gap-y-[12px] md:gap-y-[15px] pb-[20px] md:pb-[24px]">
+                <div className="animate w-full h-[20px] md:h-[24px]"></div>
+                <div className="animate w-full h-[20px] md:h-[24px]"></div>
+                <div className="animate w-full h-[20px] md:h-[24px]"></div>
               </div>
-              <div
-                key="2"
-                className="border-[#282C2B]  border-b-[1px] flex flex-col gap-y-[12px] md:gap-y-[15px] pt-[18.5px] md:pt-[23px] pb-[20px] md:pb-[24px]"
-              >
-                <div className="animate w-full h-[20px] md:h-[22px]"></div>
-                <div className="animate w-full h-[20px] md:h-[22px]"></div>
-                <div className="animate w-full h-[20px] md:h-[22px]"></div>
-                <div className="animate w-full h-[20px] md:h-[22px]"></div>
+              <div className="border-[#282C2B]  border-b-[1px] flex flex-col gap-y-[12px] md:gap-y-[15px] pt-[18.5px] md:pt-[23px] pb-[20px] md:pb-[24px]">
+                <div className="animate w-full h-[20px] md:h-[24px]"></div>
+                <div className="animate w-full h-[20px] md:h-[24px]"></div>
+                <div className="animate w-full h-[20px] md:h-[24px]"></div>
+                <div className="animate w-full h-[20px] md:h-[24px]"></div>
               </div>
-              <div
-                key="3"
-                className="flex flex-col gap-y-[12px] md:gap-y-[15px] pt-[19px] md:pt-[24px]"
-              >
-                <div className="animate w-full h-[20px] md:h-[22px]"></div>
-                <div className="animate w-full h-[20px] md:h-[22px]"></div>
+              <div className="flex flex-col gap-y-[12px] md:gap-y-[15px] pt-[19px] md:pt-[24px]">
+                <div className="animate w-full h-[20px] md:h-[24px]"></div>
+                <div className="animate w-full h-[20px] md:h-[24px]"></div>
               </div>
-              <div
-                key="4"
-                className="font-space flex flex-wrap items-center pt-[31px] gap-[10px] gap-y-[13px] md:gap-x-[17px]"
-              >
+              <div className="font-space flex flex-wrap items-center pt-[31px] gap-[10px] gap-y-[13px] md:gap-x-[17px]">
                 <div className="animate w-full h-[33px] md:h-[38px]"></div>
               </div>
             </div>
           </div>
-          <div key="2" className="panel-custom mt-[31px]">
+          <div className="panel-custom mt-[31px]">
             <div className="font-space text-lg md:text-[20px] leading-[23px] md:leading-[26px] px-[15px] py-[19px] md:px-[30px] md:pt-[23px] md:pb-[20px]  border-b-[1px] border-[#282C2B] border-solid px-[15px] uppercase">
               Escrowed <span className="uppercase">TENDIES</span>
             </div>
             <div className="px-[15px] pt-[20px] pb-[15.9px] md:px-[30px] md:pt-[23px] md:pb-[30px] text-sm leading-5 md:text-base md:leading-[22px]">
-              <div
-                key="1"
-                className="border-[#282C2B] border-b-[1px] flex flex-col gap-y-[12px] md:gap-y-[15px] pb-[19px] md:pb-[23px] "
-              >
-                <div className="animate w-full h-[20px] md:h-[22px]"></div>
-                <div className="animate w-full h-[20px] md:h-[22px]"></div>
-                <div className="animate w-full h-[20px] md:h-[22px]"></div>
+              <div className="border-[#282C2B] border-b-[1px] flex flex-col gap-y-[12px] md:gap-y-[15px] pb-[19px] md:pb-[23px] ">
+                <div className="animate w-full h-[20px] md:h-[24px]"></div>
+                <div className="animate w-full h-[20px] md:h-[24px]"></div>
+                <div className="animate w-full h-[20px] md:h-[24px]"></div>
               </div>
-              <div
-                key="2"
-                className="border-[#282C2B]  border-b-[1px] flex flex-col gap-y-[12px] md:gap-y-[15px] pt-[13px] pb-[20px] md:pt-[24px] md:pb-[23px] "
-              >
-                <div className="animate w-full h-[20px] md:h-[22px]"></div>
-                <div className="animate w-full h-[20px] md:h-[22px]"></div>
+              <div className="border-[#282C2B]  border-b-[1px] flex flex-col gap-y-[12px] md:gap-y-[15px] pt-[13px] pb-[20px] md:pt-[24px] md:pb-[23px] ">
+                <div className="animate w-full h-[20px] md:h-[24px]"></div>
+                <div className="animate w-full h-[20px] md:h-[24px]"></div>
               </div>
-              <div
-                key="3"
-                className="flex flex-col gap-y-[12px] md:gap-y-[15px] pt-[20px] md:pt-[24px]"
-              >
-                <div className="animate w-full h-[20px] md:h-[22px]"></div>
-                <div className="animate w-full h-[20px] md:h-[22px]"></div>
+              <div className="flex flex-col gap-y-[12px] md:gap-y-[15px] pt-[20px] md:pt-[24px]">
+                <div className="animate w-full h-[20px] md:h-[24px]"></div>
+                <div className="animate w-full h-[20px] md:h-[24px]"></div>
               </div>
-              <div
-                key="4"
-                className="font-space flex flex-wrap items-center pt-[32px] gap-[12px] gap-y-[13px] md:gap-x-[17px]"
-              >
+              <div className="font-space flex flex-wrap items-center pt-[32px] gap-[12px] gap-y-[13px] md:gap-x-[17px]">
                 <div className="animate w-full h-[33px] md:h-[38px]"></div>
               </div>
             </div>
