09a015f0bc7010c50e40c6b13d7f29e761f4f84f
Stefan <stefanv407@gmail.com>
2022-10-25 19:42:47 +0300

    added some changes on the token page

--- app/components/token-page/tokenTopDetailsEmpty.tsx
+++ app/components/token-page/tokenTopDetailsEmpty.tsx
@@ -1,26 +1,26 @@
 function TokenTopDetailsEmpty() {
   return (
-    <div className="flex flex-col ml-[15px] mr-[15px] md:flex-row md:justify-between md:mr-[30px] md:ml-[30px] mb-[30px] md:mb-[50px]">
-      <div className="flex items-center md:w-[30%] mb-[30px] md:mb-0">
+    <div className="flex flex-col ml-[15px] mr-[15px] sm:flex-row md:justify-between md:mr-[30px] md:ml-[30px] mb-[30px] md:mb-[50px]">
+      <div className="flex items-center sm:w-[30%] mb-[30px] sm:mb-0 mr-0 md:mr-[30px]">
         <div className="animate w-10 h-10 mr-[15px] md:w-[55px] md:h-[55px] md:mr-[21px]"></div>
         <div className="animate w-[70px] h-[25px] md:w-[90px] md:h-[31px]"></div>
       </div>
-      <div className="flex md:gap-[0px] gap-x-[0px] gap-y-[10px] flex-wrap font-nova md:flex-nowrap justify-start md:justify-between md:text-center w-full md:w-[511px]">
-        <div className="w-[120px] md:w-[auto]">
-          <div className="animate w-[100px] h-[14px] mb-[5px] md:h-[22px]"></div>
-          <div className="animate w-[100px] md:w-[100px] h-[19px] md:h-[28px]"></div>
+      <div className="flex font-nova justify-between md:text-center w-full md:w-[511px]">
+        <div className="w-[70px] md:w-[auto]">
+          <div className="animate w-[70px] h-[14px] mb-[5px] md:h-[22px]"></div>
+          <div className="animate w-[70px] md:w-[100px] h-[19px] md:h-[28px]"></div>
         </div>
-        <div className="w-[120px] md:w-[auto]">
-          <div className="animate w-[100px] h-[14px] mb-[5px] md:h-[22px]"></div>
-          <div className="animate w-[100px] md:w-[100px] h-[19px] md:h-[28px]"></div>
+        <div className="w-[70px] md:w-[auto]">
+          <div className="animate w-[70px] h-[14px] mb-[5px] md:h-[22px]"></div>
+          <div className="animate w-[70px] md:w-[100px] h-[19px] md:h-[28px]"></div>
         </div>
-        <div className="w-[120px] md:w-[auto]">
-          <div className="animate w-[100px] h-[14px] mb-[5px] md:h-[22px]"></div>
-          <div className="animate w-[100px] md:w-[100px] h-[19px] md:h-[28px]"></div>
+        <div className="w-[70px] md:w-[auto]">
+          <div className="animate w-[70px] h-[14px] mb-[5px] md:h-[22px]"></div>
+          <div className="animate w-[70px] md:w-[100px] h-[19px] md:h-[28px]"></div>
         </div>
-        <div className="w-[120px] md:w-[auto]">
-          <div className="animate w-[100px] h-[14px] mb-[5px] md:h-[22px]"></div>
-          <div className="animate w-[100px] md:w-[100px] h-[19px] md:h-[28px]"></div>
+        <div className="w-[70px] md:w-[auto]">
+          <div className="animate w-[70px] h-[14px] mb-[5px] md:h-[22px]"></div>
+          <div className="animate w-[70px] md:w-[100px] h-[19px] md:h-[28px]"></div>
         </div>
       </div>
     </div>
