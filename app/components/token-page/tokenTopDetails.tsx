0977dd6b94ea8d79cb78518b1a13bd254d18cdba
sandeep mt <mtsandeep@gmail.com>
2022-11-13 08:55:44 +0530

    remove borrow apy from markets

--- app/components/token-page/tokenTopDetails.tsx
+++ app/components/token-page/tokenTopDetails.tsx
@@ -10,9 +10,8 @@ function TokenTopDetails({ marketInfo }: { marketInfo: object | boolean }) {
     open: false,
     coins: [{}],
   });
-
-  const borrowApy =
-    marketInfo.tokenSymbol === "GLP" ? 0 : marketInfo.borrowApy * -1;
+  const isBorrowable = marketInfo.tokenSymbol !== "GLP";
+  const borrowApy = isBorrowable ? marketInfo.borrowApy * -1 : 0;
 
   const supplyApy = marketInfo.supplyApy;
 
@@ -39,7 +38,11 @@ function TokenTopDetails({ marketInfo }: { marketInfo: object | boolean }) {
           </p>
         </div>
 
-        <div className="flex font-nova justify-between md:text-center w-full md:w-[511px]">
+        <div
+          className={`flex font-nova justify-between md:text-center md:w-[${
+            isBorrowable ? "511px" : "242px"
+          }]`}
+        >
           <div className="w-[auto]">
             <div
               tabIndex={0}
@@ -128,94 +131,98 @@ function TokenTopDetails({ marketInfo }: { marketInfo: object | boolean }) {
               {`$${toShortFiatString(marketInfo.totalSupplyUSD)} USD`}
             </p>
           </div>
-          <div className="w-[auto]">
-            <div
-              tabIndex={0}
-              className="relative flex flex-col items-start md:items-center group"
-              onClick={() =>
-                setMultiTooltipData({
-                  ...multiTooltipData,
-                  open: window.innerWidth < 1023,
-                  coins: [
-                    {
-                      coinTitle: marketInfo.tokenSymbol,
-                      iconSrc: marketInfo.icon,
-                      data: formatApy(borrowApy),
-                      color: checkColorClass(borrowApy),
-                    },
-                    {
-                      coinTitle: "esTND",
-                      iconSrc: "/images/wallet-icons/balance-icon.svg",
-                      data: "0.00%",
-                      color: "text-white",
-                    },
-                  ],
-                })
-              }
-            >
-              <p className="text-[10px] text-[#818987] leading-[14px] font-semibold underline decoration-dashed underline-offset-[2px] mb-[4px] md:text-sm md:leading-[19px] cursor-pointer">
-                Borrow APY
-              </p>
-              <div className="hidden flex-col absolute bottom__custom items-center group-hover:hidden lg:group-hover:flex group-focus:flex rounded-[10px]">
-                <div className="relative z-10 leading-none whitespace-no-wrap shadow-lg w-[100%] mx-[0px] !rounded-[10px] panel-custom">
-                  <div className="flex-col w-full h-full bg-[#181D1B] shadow-lg rounded-[10px] pt-[14px] pr-[16px] pb-[14px] pl-[16px]">
-                    <div className="flex justify-between gap-[30px] mb-[12px] last:mb-[0]">
-                      <div className="flex gap-[8px]">
-                        <img
-                          className="max-w-[18px]"
-                          src={marketInfo.icon}
-                          alt="..."
-                        />
-                        <span className="font-nova text-white text-sm font-normal">
-                          {marketInfo.tokenSymbol}
+          {isBorrowable && (
+            <div className="w-[auto]">
+              <div
+                tabIndex={0}
+                className="relative flex flex-col items-start md:items-center group"
+                onClick={() =>
+                  setMultiTooltipData({
+                    ...multiTooltipData,
+                    open: window.innerWidth < 1023,
+                    coins: [
+                      {
+                        coinTitle: marketInfo.tokenSymbol,
+                        iconSrc: marketInfo.icon,
+                        data: formatApy(borrowApy),
+                        color: checkColorClass(borrowApy),
+                      },
+                      {
+                        coinTitle: "esTND",
+                        iconSrc: "/images/wallet-icons/balance-icon.svg",
+                        data: "0.00%",
+                        color: "text-white",
+                      },
+                    ],
+                  })
+                }
+              >
+                <p className="text-[10px] text-[#818987] leading-[14px] font-semibold underline decoration-dashed underline-offset-[2px] mb-[4px] md:text-sm md:leading-[19px] cursor-pointer">
+                  Borrow APY
+                </p>
+                <div className="hidden flex-col absolute bottom__custom items-center group-hover:hidden lg:group-hover:flex group-focus:flex rounded-[10px]">
+                  <div className="relative z-10 leading-none whitespace-no-wrap shadow-lg w-[100%] mx-[0px] !rounded-[10px] panel-custom">
+                    <div className="flex-col w-full h-full bg-[#181D1B] shadow-lg rounded-[10px] pt-[14px] pr-[16px] pb-[14px] pl-[16px]">
+                      <div className="flex justify-between gap-[30px] mb-[12px] last:mb-[0]">
+                        <div className="flex gap-[8px]">
+                          <img
+                            className="max-w-[18px]"
+                            src={marketInfo.icon}
+                            alt="..."
+                          />
+                          <span className="font-nova text-white text-sm font-normal">
+                            {marketInfo.tokenSymbol}
+                          </span>
+                        </div>
+                        <span
+                          className={`font-nova text-sm font-normal ${checkColorClass(
+                            borrowApy
+                          )}`}
+                        >
+                          {formatApy(borrowApy)}
                         </span>
                       </div>
-                      <span
-                        className={`font-nova text-sm font-normal ${checkColorClass(
-                          borrowApy
-                        )}`}
-                      >
-                        {formatApy(borrowApy)}
-                      </span>
-                    </div>
-                    <div className="flex justify-between gap-[30px]">
-                      <div className="flex gap-[8px]">
-                        <img
-                          className="max-w-[18px]"
-                          src="/images/wallet-icons/balance-icon.svg"
-                          alt="..."
-                        />
+                      <div className="flex justify-between gap-[30px]">
+                        <div className="flex gap-[8px]">
+                          <img
+                            className="max-w-[18px]"
+                            src="/images/wallet-icons/balance-icon.svg"
+                            alt="..."
+                          />
+                          <span className="font-nova text-white text-sm font-normal">
+                            esTND
+                          </span>
+                        </div>
                         <span className="font-nova text-white text-sm font-normal">
-                          esTND
+                          0.00%
                         </span>
                       </div>
-                      <span className="font-nova text-white text-sm font-normal">
-                        0.00%
-                      </span>
                     </div>
                   </div>
+                  <div className="custom__arrow__tooltip relative top-[-6px] left-[0.5px] w-3 h-3 rotate-45 bg-[#181D1B]"></div>
                 </div>
-                <div className="custom__arrow__tooltip relative top-[-6px] left-[0.5px] w-3 h-3 rotate-45 bg-[#181D1B]"></div>
               </div>
+              <p
+                className={`text-sm font-medium leading-[19px] md:text-[22px] md:leading-[31px] ${
+                  checkZeroValue(borrowApy)
+                    ? "text-white"
+                    : checkColorClass(borrowApy)
+                }`}
+              >
+                {formatApy(borrowApy)}
+              </p>
             </div>
-            <p
-              className={`text-sm font-medium leading-[19px] md:text-[22px] md:leading-[31px] ${
-                checkZeroValue(borrowApy)
-                  ? "text-white"
-                  : checkColorClass(borrowApy)
-              }`}
-            >
-              {formatApy(borrowApy)}
-            </p>
-          </div>
-          <div className="w-[auto]">
-            <p className="text-[10px] text-[#818987] leading-[14px] font-semibold md:text-sm md:leading-[19px] mb-[4px]">
-              Total Borrow
-            </p>
-            <p className="mt-[4px] text-sm font-medium leading-[19px] md:text-[22px] md:leading-[31px]">
-              {`$${toShortFiatString(marketInfo.totalBorrowUSD)} USD`}
-            </p>
-          </div>
+          )}
+          {isBorrowable && (
+            <div className="w-[auto]">
+              <p className="text-[10px] text-[#818987] leading-[14px] font-semibold md:text-sm md:leading-[19px] mb-[4px]">
+                Total Borrow
+              </p>
+              <p className="mt-[4px] text-sm font-medium leading-[19px] md:text-[22px] md:leading-[31px]">
+                {`$${toShortFiatString(marketInfo.totalBorrowUSD)} USD`}
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </>
