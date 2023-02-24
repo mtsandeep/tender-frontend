7ff7b4f9c5886655f0fd2690454523c3ce1dea69
Eli <eli.sakov@hotmail.com>
2023-02-23 21:07:10 -0800

    fix price in usdc

--- app/routes/api/tnd_price.ts
+++ app/routes/api/tnd_price.ts
@@ -5,9 +5,9 @@ let contract = Tendies.Tokens.TND.address
 
 export const loader:LoaderFunction = async ({ request }) => {
     let response = await fetch(`https://api.coingecko.com/api/v3/simple/token_price/arbitrum-one?contract_addresses=${contract}&vs_currencies=usd`)
-    let { usd } = await response.json() as {[contract: string]: {"usd": number}}
+    let data = await response.json() as {[contract: string]: {"usd": number}}
     
-    return json(usd, {
+    return json(data[contract].usd, {
         headers: {
             "Cache-Control": "max-age=45, public",
         },
